267a94025661c1fe45fdd4c3da84a37de8293cb8
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2022-12-05 16:45:54 +0800

    create class node

--- Exercise_4/Program.cs
+++ Exercise_4/Program.cs
@@ -1,4 +1,13 @@
 ﻿namespace Exercise_4
 {
-
+    class node
+    {
+        public int info;
+        public node next;
+        public node(int i, node n)
+        {
+            info = i;
+            next = n;
+        }
+    }
 }
