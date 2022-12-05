d9705c1a45bc272fbafc5fc4cb82cafdeb82ceeb
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2022-12-05 17:24:01 +0800

    create public void push

--- Exercise_4/Program.cs
+++ Exercise_4/Program.cs
@@ -26,5 +26,14 @@
             else
                 return (false);
         }
+        public void push(int element)
+        {
+            node fresh;
+            fresh = new node(element, null);
+
+            fresh.next = top;
+            top = fresh;
+            Console.WriteLine("\n" + element + "pushed");
+        }
     }
 }
